8c297dc5ec980ce05816adc5af6b8e9e50dc9c92
Steve Schwedt <sschwedt@gmail.com>
2021-01-02 10:16:27 -0700

    Shows posts index

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'create post' do
+  it 'can create a post' do
+    visit new_post_path
+
+    expect(current_path).to eq(new_post_path)
+    expect(Post.count).to eq(0)
+
+    fill_in 'post[title]', with: 'Cool Post'
+    fill_in 'post[body]', with: 'Whoa yeah!'
+
+    click_on 'Create Post'
+
+    expect(Post.count).to eq(1)
+    post = Post.first
+    expect(current_path).to eq(post_path(post))
+
+    expect(page).to have_content('Cool Post')
+    expect(page).to have_content('Whoa yeah!')
+  end
+end
+
+describe 'post index' do
+  it 'can show all posts' do
+    post1 = Post.create(title: 'Cool Post', body: 'Whoa yeah!')
+    post2 = Post.create(title: 'Super Post', body: 'Yow!!!')
+
+    visit posts_path
+
+    expect(page).to have_content('Cool Post')
+    expect(page).to have_content('Super Post')
+
+    click_on 'Cool Post'
+    expect(current_path).to eq(post_path(post1))
+
+    click_on 'Blogzilla'
+    expect(current_path).to eq(posts_path)
+
+    click_on 'Super Post'
+    expect(current_path).to eq(post_path(post2))
+  end
+end
